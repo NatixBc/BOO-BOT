21c3fb92deff10ebe79bf51edcb49816b81550be
NatixBc <144946924+NatixBc@users.noreply.github.com>
2023-09-26 21:10:56 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -17,7 +17,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  m.reply(`╭「*CR-BOT*」
+  m.reply(`╭「𝙈𝙞𝙢𝙞 𝘽𝙖𝙗𝙚」
 │➯⛥❏[🌴] *HOLA YA ESTAS REGISTRADO*
 │➯⛥❏[🤴] *NOMBRE* ${name}
 │➯⛥❏[🏧] *EDAD* ${age} AÑOS 
