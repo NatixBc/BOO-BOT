63bd8544426ed67c2654adac58fa9e905104e24e
NatixBc <144946924+NatixBc@users.noreply.github.com>
2023-09-26 21:01:16 -0500

    Update menu+18.js

--- plugins/menu+18.js
+++ plugins/menu+18.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
 try {
-await m.reply(`╭「*CR-BOT*」\n│➯(♦️)*Hola estás en el menú+18 elige una opción y pide una imagen*\n╰───────────────╯\n\n\n╭「*CR-BOT*」\n│➯(♦️)*Hello you are in the menu+18 choose an option and ask for an image*\n╰───────────────╯`)
+await m.reply(`╭「𝙈𝙞𝙢𝙞 𝘽𝙖𝙗𝙚」\n│➯(♦️)*Hola estás en el menú+18 elige una opción y pide una imagen*\n╰───────────────╯\n\n\n╭「𝙈𝙞𝙢𝙞 𝘽𝙖𝙗𝙚」\n│➯(♦️)*Hello you are in the menu+18 choose an option and ask for an image*\n╰───────────────╯`)
 let pp = imagen4
 let img = await(await fetch('https://i.imgur.com/JP52fdP.jpg')).buffer()
 let d = new Date(new Date + 3600000)
@@ -17,16 +17,16 @@ let rtotalreg = Object.values(global.db.data.users).filter(user => user.register
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850)   
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `╭「*CR-BOT*」
+let str = `╭「𝙈𝙞𝙢𝙞 𝘽𝙖𝙗𝙚」
 │➯*HOLA:*➟${taguser}
-│➯*CREADOR:*: CR
-│➯*CR-BOT*
+│➯*CREADOR:*: Babbi
+│➯*𝙈𝙞𝙢𝙞 𝘽𝙖𝙗𝙚*
 │➯*FECHA:*➟${date}
 │➯*TIEMPO ACTIVO:*➟${uptime}
 │➯*USUARIOS*➟${rtotalreg}
 ︎╰───────────────╯
 🍁᭢━━━━━━━━━᭥🍁᭢
-╭「*𝕾𝖔𝖒𝖔𝖘 𝕱𝖆𝖒𝖎𝖑𝖎𝖆 𝕾𝖔𝖒𝖔𝖘 𝕷𝖊𝖌𝖎ó𝖓*」
+╭「𝙈𝙞𝙢𝙞 𝘽𝙖𝙗𝙚」
 │➯*Menu*+18*
 ︎╰───────────────╯
 🍁᭢━━━━━━━━━᭥🍁᭢
